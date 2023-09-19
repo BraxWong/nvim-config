35ae3a251c6fda77a0eba28bb2fafd8007be8717
Brax Wong <ghostenjob@gmail.com>
2023-09-19 21:34:33 +0100

    Updating Nvim config to windows.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,5 +3,8 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
+    keys = {
+      { "<leader>qr", "<cmd>TodoLocList<cr>", desc = "TodoLocList" },
+    },
   },
 }
